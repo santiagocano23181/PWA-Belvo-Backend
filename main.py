62797ec729c0c3654be0668142660241895e5edd
Santiago Cano Ospina <sc@smartalentit.com>
2025-01-21 09:06:53 -0500

    Auth correction

--- main.py
+++ main.py
@@ -51,8 +51,9 @@ def session_middleware():
     method = request.method
     if method != 'OPTIONS':
         auth = request.headers.get('Authorization')
-        token = auth.split(" ")[1]
+
         if auth:
+            token = auth.split(" ")[1]
             value = jwt.decode(token, env('SECRET_KEY'),
                                algorithms=['HS256'])
             session['Authorization'] = value['id']
@@ -67,8 +68,8 @@ def audit_middleware():
     method = request.method
     if method != 'OPTIONS':
         auth = request.headers.get('Authorization')
-        token = auth.split(" ")[1]
         if auth:
+            token = auth.split(" ")[1]
             value = jwt.decode(token, env('SECRET_KEY'),
                                algorithms=['HS256'])
             new_event = UserHistory.UserHistory(value['id'], f'{method} {request.path}')
