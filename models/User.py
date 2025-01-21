17bd1b9a62a33ce8517359437f7dafe2ef3929e7
Santiago Cano Ospina <sc@smartalentit.com>
2025-01-20 21:40:45 -0500

    autoincremental

--- models/User.py
+++ models/User.py
@@ -4,7 +4,7 @@ from utils.db import db
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(255), unique=True, nullable=True)
     password = db.Column(db.String(255), nullable=False)
 
--- models/UserHistory.py
+++ models/UserHistory.py
@@ -7,7 +7,7 @@ from utils.db import db
 class UserHistory(db.Model):
     __tablename__ = 'userHistory'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     user = db.relationship('User', backref=db.backref('userHistory', lazy=True))
